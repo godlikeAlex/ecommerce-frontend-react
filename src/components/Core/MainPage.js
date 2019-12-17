8ad2a81cf93adc699119acd4d91d944a632278ae
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-12-17 23:45:00 +0500

    added methods for get all products and simple stringify it in view

--- src/components/Core/MainPage.js
+++ src/components/Core/MainPage.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
+import {getProducts} from './ApiCore';
 
 const MainPage = () => {
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productByArrival, setProductByArrival] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if(data.err) {
+                setError(data.err);
+            } else {
+                setProductsBySell(data);
+            }
+        })
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            if(data.err) {
+                setError(data.err);
+            } else {
+                setProductByArrival(data);
+            }
+        })
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
     return (
         <div>
             <Layout title="Home Page" description="Node React Ecomerce application">
-                ...
+                {JSON.stringify(productByArrival)}
+                <hr />
+                {JSON.stringify(productsBySell)}
             </Layout>
         </div>
     )
