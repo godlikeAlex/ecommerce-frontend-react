d391f3ea86b3bcb17ddbb7cf4babf67745d0be40
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-12-19 21:06:55 +0500

    handle checked category

--- src/components/Core/CheckBox.js
+++ src/components/Core/CheckBox.js
@@ -1,9 +1,23 @@
 import React, {useState, useEffect} from "react";
 
 const CheckBox = ({categories}) => {
+    const [checked, setChecked] = useState([]);
+
+    const handleChange = category => () => {
+        const currentCategory = checked.indexOf(category);
+        const newCheckedCategory = [...checked];
+
+        if(currentCategory === -1) {
+            newCheckedCategory.push(category);
+        } else {
+            newCheckedCategory.splice(currentCategory, 1);
+        }
+        setChecked(newCheckedCategory);
+    };
+
     return categories.map((category, i) => (
         <li key={i} className="list-unstyled">
-            <input type="checkbox" className="from-check-input" />
+            <input type="checkbox" onChange={handleChange(category._id)} value={checked.indexOf(category._id === -1)} className="from-check-input" />
             <label className="from-check-label">{category.name}</label>
         </li>
     ))
